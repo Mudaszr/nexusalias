cbe934e4eefcb7b16537440e033f4176176d73b2
Mudaszr <mudaszr@gmail.com>
2026-05-10 14:36:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,17 @@
-
+export default function Home() {
+  return (
+    <div
+      style={{
+        background: "black",
+        color: "white",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+      }}
+    >
+      NexusAlias
+    </div>
+  )
+}
