b5c17383b0e7c88c53601be800d3079aab102101
Mudaszr <mudaszr@gmail.com>
2026-05-10 17:05:33 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "NexusAlias"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
