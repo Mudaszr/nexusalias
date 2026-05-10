bd7722140b6b824465d15e99de0b49e4edf8d54b
Mudaszr <mudaszr@gmail.com>
2026-05-10 20:03:09 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        style={{
+          margin: 0,
+          padding: 0,
+          background: "#060606",
+          overflow: "hidden"
+        }}
+      >
+        {children}
+      </body>
     </html>
   )
 }
