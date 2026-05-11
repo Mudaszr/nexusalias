192f26929e0373a4ca079631e51406a4ea994a90
Mudaszr <mudaszr@gmail.com>
2026-05-11 23:51:52 +0530

    Create event.js

---
+++ data/event.js
@@ -0,0 +1,46 @@
+export const events = [
+  {
+    id: 1,
+
+    title: "Red Sea Shipping Disruption",
+
+    severity: "high",
+
+    category: "Shipping",
+
+    sourceCountry: "Yemen",
+
+    affectedCountry: "Europe",
+
+    from: [48.5164, 15.5527],
+    to: [10.4515, 51.1657],
+
+    impact: [
+      "Shipping costs increased",
+      "Oil transport delays",
+      "European inflation pressure"
+    ],
+  },
+
+  {
+    id: 2,
+
+    title: "Taiwan Semiconductor Risk",
+
+    severity: "medium",
+
+    category: "Technology",
+
+    sourceCountry: "Taiwan",
+
+    affectedCountry: "United States",
+
+    from: [121.5654, 25.033],
+    to: [-95.7129, 37.0902],
+
+    impact: [
+      "Chip supply concerns",
+      "Tech manufacturing delays"
+    ],
+  },
+];
